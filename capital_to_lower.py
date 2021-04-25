c0961ed1661a71db6450c68cae21a59babf7703c
Yoshiaki Asahara <ashr@YoshiakinoiMac.local>
2021-04-25 14:35:43 -0700

    use Getch instead of input

--- capital_to_lower.py
+++ capital_to_lower.py
@@ -3,17 +3,20 @@ import os
 import sys
 import glob
 import shutil
+from getch import Getch
 
 if __name__ == "__main__":
     file_list = glob.glob("./*")
+    getch = Getch()
     for file in file_list:
         file_name = file.replace("./", "")
         new_name = file_name.replace(file_name, file_name.lower())
         if (new_name == file_name):
             continue
-        key = input("Rename from [" + file_name + "]\n"
-                    + "         to [" + new_name + "]?  Yes:Enter  No:else ")
-        if key == "":
+        print("Rename from [" + file_name + "]\n"
+              + "         to [" + new_name + "]?  y/n")
+        key = getch()
+        if key == 'y':
             os.rename(file_name, new_name)
-        else:
+        elif key == 'n':
             print("skip rename [" + file_name + "]")
