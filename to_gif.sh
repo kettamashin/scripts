df4d9fb948074f6a7669e9cea452ee58ed1ea963
Yoshiaki Asahara <yoshiaki.asahara@tri.global>
2021-06-20 14:18:39 -0700

    ignore overwriting

--- to_gif.sh
+++ to_gif.sh
@@ -5,5 +5,5 @@ if [ $# -ne 4 ]; then
 fi
 ffmpeg -ss $2 -t $3 -i $1 -vf scale=$4:-1,fps=20 $1.scaled.mp4
 ffmpeg -i $1.scaled.mp4 -vf palettegen $1.png
-ffmpeg -i $1.scaled.mp4 -i $1.png -filter_complex paletteuse $1.gif
+ffmpeg -i $1.scaled.mp4 -i $1.png -filter_complex paletteuse $1.gif -y
 rm -f $1.png $1.scaled.mp4
