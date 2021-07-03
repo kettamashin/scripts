58a1bbfc254f7c0ea02045ae995bd0014c62ab1c
Yoshiaki Asahara <ketta.mashin@gmail.com>
2021-07-03 16:38:15 -0700

    add delete function

--- find_same_size_file.py
+++ find_same_size_file.py
@@ -1,16 +1,43 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import glob
 import subprocess
 import os
 import random
+import subprocess
+from getch import Getch
+from send2trash import send2trash
 
+getch = Getch()
 file_list = sorted(glob.glob("./*"), key=os.path.getsize)
-pre_size = ""
+pre_file = ''
+pre_size = ''
 
-for file_name in file_list:
-    cur_size = os.path.getsize(file_name)
-    if cur_size == pre_size:
-        print(str(cur_size/1000000) +
-              " MB [" + file_name + "] is same as previous file.")
-    pre_size = cur_size
+for file in file_list:
+    size = os.path.getsize(file)
+
+    if size == pre_size:
+        print('['+pre_file+']')
+        print('['+file+']')
+        print('    are same size files (' + str(size/1000000) + 'MB)\n')
+
+        print('/:open')
+        print('\':skip')
+        print('k:delete [' + pre_file + ']')
+        print('l:delete [' + file + ']')
+        while True:
+            key = getch()
+            if(key == '/'):
+                subprocess.call(
+                    ["open", "/Applications/VLC.app", file, pre_file])
+            elif(key == '\''):
+                break
+            elif(key == 'k'):
+                send2trash(pre_file)
+                break
+            elif(key == 'l'):
+                send2trash(file)
+                break
+
+    pre_file = file
+    pre_size = size
