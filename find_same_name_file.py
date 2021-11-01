cb06e21b22fe385b5bb24c70d8a88e971038ded4
Yoshiaki Asahara <ketta.mashin@gmail.com>
2021-10-31 21:32:18 -0700

    fix bug

--- find_same_name_file.py
+++ find_same_name_file.py
@@ -27,14 +27,18 @@ for path in paths:
     files += glob.glob(path + '/*')
 
 ref_files = files[:]
+deleted_files = []
 
 for file in files:
     for ref_file in ref_files:
         if file == ref_file:
-            break
+            continue
         if os.path.basename(file) != os.path.basename(ref_file):
-            break
-
+            continue
+        if os.path.basename(file) in deleted_files:
+            continue
+        if os.path.basename(ref_file) in deleted_files:
+            continue
         print('-----------------------')
         print('[size=' + str(os.path.getsize(file)) + ' ' + file + ']')
         print('[size=' + str(os.path.getsize(ref_file)) + ' ' + ref_file + ']')
@@ -44,16 +48,21 @@ for file in files:
               str(os.path.getsize(file)) + ' ' + file + ']')
         print(
             '  l:delete [size=' + str(os.path.getsize(ref_file)) + ' ' + ref_file + ']')
+        print('  q:quit')
         while True:
             key = getch()
             if(key == '/'):
                 subprocess.call(
-                    ["open", "/Applications/VLC.app", file, ref_file])
+                    ["open", "/Applications/VLC.app", ref_file, file])
             elif(key == '\''):
                 break
             elif(key == 'k'):
                 send2trash(file)
+                deleted_files.append(os.path.basename(file))
                 break
             elif(key == 'l'):
                 send2trash(ref_file)
+                deleted_files.append(os.path.basename(ref_file))
                 break
+            elif(key == 'q'):
+                exit()
