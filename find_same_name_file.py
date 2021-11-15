517bf14cd10f498a8ec6974d9b4329933069d1f6
Yoshiaki Asahara <ketta.mashin@gmail.com>
2021-11-14 20:16:50 -0800

    add percent print

--- find_same_name_file.py
+++ find_same_name_file.py
@@ -28,8 +28,14 @@ for path in paths:
 files2 = files1[:]
 deleted_files = []
 
+num_of_pairs = len(files1) ** 2
+counter = 0
+
 for file1 in files1:
     for file2 in files2:
+        counter += 1
+        print('\r' + '{:.1f}'.format(counter / num_of_pairs * 100) +
+              '% processed...', end='')
         if file1 == file2:
             continue
         if os.path.basename(file1) != os.path.basename(file2):
@@ -38,6 +44,7 @@ for file1 in files1:
             continue
         if os.path.basename(file2) in deleted_files:
             continue
+
         print('-----------------------')
         print('[size=' + str(os.path.getsize(file1)) + ' ' + file1 + ']')
         print('[size=' + str(os.path.getsize(file2)) + ' ' + file2 + ']')
